e72b26b2da02b2e8b2f705c1d08c21b186894f28
joaomarcelofa <joaov7.1996@gmail.com>
2018-05-26 21:16:19 -0300

    Activity do esquema do jogo

--- gen/com/example/tictactoerampage/R.java
+++ gen/com/example/tictactoerampage/R.java
@@ -10,6 +10,12 @@ package com.example.tictactoerampage;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int app_background=0x7f040003;
+        public static final int display_color=0x7f040002;
+        public static final int grid_color=0x7f040000;
+        public static final int stick_color=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,25 +24,64 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int default_margin=0x7f050003;
+        public static final int display_font_size=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int circle=0x7f020000;
+        public static final int cross=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f0a0017;
+        public static final int block_1=0x7f0a0003;
+        public static final int block_10=0x7f0a000c;
+        public static final int block_11=0x7f0a000d;
+        public static final int block_12=0x7f0a000e;
+        public static final int block_13=0x7f0a000f;
+        public static final int block_14=0x7f0a0010;
+        public static final int block_15=0x7f0a0011;
+        public static final int block_16=0x7f0a0012;
+        public static final int block_2=0x7f0a0004;
+        public static final int block_3=0x7f0a0005;
+        public static final int block_4=0x7f0a0006;
+        public static final int block_5=0x7f0a0007;
+        public static final int block_6=0x7f0a0008;
+        public static final int block_7=0x7f0a0009;
+        public static final int block_8=0x7f0a000a;
+        public static final int block_9=0x7f0a000b;
+        public static final int exit=0x7f0a0000;
+        public static final int jogador_vez_label=0x7f0a0002;
+        public static final int player1_label=0x7f0a0013;
+        public static final int player1_points=0x7f0a0014;
+        public static final int player2_label=0x7f0a0015;
+        public static final int player2_points=0x7f0a0016;
+        public static final int replay_button=0x7f0a0001;
+    }
+    public static final class integer {
+        public static final int qtd_colunas=0x7f060001;
+        public static final int qtd_linhas=0x7f060000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_game=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int game=0x7f090000;
+        public static final int main=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f070002;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070001;
+        public static final int jogador_bola_vez=0x7f070008;
+        public static final int jogador_cruz_vez=0x7f070007;
+        public static final int menu=0x7f070004;
+        public static final int p1=0x7f070005;
+        public static final int p2=0x7f070006;
+        public static final int title_activity_game=0x7f070003;
     }
     public static final class style {
         /** 
@@ -60,10 +105,21 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+        public static final int DefaultStyle=0x7f080002;
+        public static final int LeftBotRowStyle=0x7f080009;
+        public static final int LeftMidRowStyle=0x7f080006;
+        public static final int LeftTopRowStyle=0x7f080003;
+        public static final int MiddleBotRowStyle=0x7f08000a;
+        public static final int MiddleMidRowStyle=0x7f080007;
+        public static final int MiddleTopRowStyle=0x7f080004;
+        public static final int RightBotRowStyle=0x7f08000b;
+        public static final int RightMidRowStyle=0x7f080008;
+        public static final int RightTopRowStyle=0x7f080005;
+        public static final int TextStyle=0x7f08000c;
     }
 }
